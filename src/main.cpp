35c109e8367523811b05f1fb381980379e58e956
Pomb24 <21021398.vnu.edu.vn>
2025-05-07 13:38:36 +0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,49 @@ unsigned long lastDataTime = 0;
 const int samplingInterval = 1000;
 
 GyverBME280 bme;
+bool sensorOK = false;
+
+void initBME280() {
+  sensorOK = bme.begin();
+  if (sensorOK) {
+    Serial.println("Đã khởi tạo BME280 thành công!");
+    bme.setHumOversampling(MODULE_DISABLE);
+    bme.setStandbyTime(STANDBY_1000MS);
+  }
+  else {
+    Serial.println("Không tìm thấy BME280!");
+  }
+}
 
 void setup(){
   Serial.begin(115200);
   Wire.begin();
-  bme.setHumOversampling(MODULE_DISABLE);       
-  bme.setStandbyTime(STANDBY_1000MS);            
-  bme.begin(); 
+
+  initBME280();
 }
 
 void loop() {
+  if (!sensorOK) {
+    initBME280();
+    delay(1000);
+    return;
+}
+
   unsigned long currentTime = millis();
 
   if (currentTime - lastDataTime >= samplingInterval) {
     lastDataTime = currentTime;
+
+    float temp = bme.readTemperature();
+    float hum = bme.readHumidity();
+    float press = bme.readPressure();
+
+    if(temp == 0 && hum == 0 && press == 0) {
+      Serial.println("Cảm biến mất kết nối!");
+      sensorOK = false;
+      return;
+    }
+
     Serial.print("Temperature: ");
     Serial.print(bme.readTemperature());      
     Serial.println(" *C");
@@ -27,6 +56,10 @@ void loop() {
     Serial.print("Pressure: ");
     Serial.print(bme.readPressure());
     Serial.println(" Pa");
-    Serial.println("");
+
+    Serial.print("Humidity: ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+    Serial.println("------------------------");
   }
 }
